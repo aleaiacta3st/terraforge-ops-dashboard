5d35fb6b8bd4c8628d3a3bcabb7eed62ba5e4340
basileus <invicinc@gmail.com>
2026-03-05 12:56:03 -0500

    Reduce Celery concurrency for Railway memory limits

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 alembic upgrade head
 python -m app.seed
-celery -A app.celery_app:celery_app worker --loglevel=info &
+celery -A app.celery_app:celery_app worker --loglevel=info --concurrency=2 --pool=solo &
 uvicorn app.main:app --host 0.0.0.0 --port 8000
