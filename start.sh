327d2f41e98351b605245cffa291f1659a7e7dd1
basileus <invicinc@gmail.com>
2026-03-06 19:31:26 -0500

    backfill embeddings

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 alembic upgrade head
 python -m app.seed
+python -m app.backfill_embeddings
 celery -A app.celery_app:celery_app worker --loglevel=info --concurrency=2 --pool=solo &
 uvicorn app.main:app --host 0.0.0.0 --port 8000
